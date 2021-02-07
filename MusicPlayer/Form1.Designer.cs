b253d7ed05711493b6399ef1a8fc70699365504a
ShayestehHS <hosseinshayesteh47.hs@gmail.com>
2021-02-07 20:45:13 +0330

    initial commit

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace MusicPlayer
+{
+    partial class frmMP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMP));
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.axWMP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnClose = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listName = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWMP)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Orange;
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(501, 409);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(339, 48);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // axWMP
+            // 
+            this.axWMP.Enabled = true;
+            this.axWMP.Location = new System.Drawing.Point(12, 64);
+            this.axWMP.Name = "axWMP";
+            this.axWMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWMP.OcxState")));
+            this.axWMP.Size = new System.Drawing.Size(471, 390);
+            this.axWMP.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Turquoise;
+            this.panel1.Controls.Add(this.BtnClose);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(852, 58);
+            this.panel1.TabIndex = 2;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.Image = global::MusicPlayer.Properties.Resources.close_x_icon_15;
+            this.BtnClose.Location = new System.Drawing.Point(799, 3);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(50, 50);
+            this.BtnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.TabStop = false;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Dima Shekasteh Free", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, -6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 82);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Music Player App";
+            // 
+            // listName
+            // 
+            this.listName.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listName.FormattingEnabled = true;
+            this.listName.ItemHeight = 19;
+            this.listName.Location = new System.Drawing.Point(501, 64);
+            this.listName.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.listName.Name = "listName";
+            this.listName.Size = new System.Drawing.Size(339, 327);
+            this.listName.TabIndex = 3;
+            this.listName.SelectedIndexChanged += new System.EventHandler(this.listName_SelectedIndexChanged);
+            // 
+            // frmMP
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(852, 466);
+            this.Controls.Add(this.listName);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.axWMP);
+            this.Controls.Add(this.btnBrowse);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWMP)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private AxWMPLib.AxWindowsMediaPlayer axWMP;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listName;
+        private System.Windows.Forms.PictureBox BtnClose;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class frmMP : Form
+    {
+        string[] Path;
+        string[] FileName;
+
+        public frmMP()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Audio (*.mp3,*.acc,*wma)|*.acc;*.mp3;*.wma|All Files (*.*)|*.*";
+            OPF.Multiselect = true;
+            OPF.Title = "Select One or Multiple Song";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                FileName = OPF.SafeFileNames;
+                Path = OPF.FileNames;
+                for (int i = 0; i < FileName.Length; i++)
+                {
+                    if (FileName[i].Contains("________________________________"))
+                    {
+                        MessageBox.Show("Please select a music that doesnt contain '_____'");
+                    }
+                    else {
+                        listName.Items.Add(FileName[i]);
+                        listName.Items.Add("________________________________");
+                    }
+
+                }
+            }
+        }
+
+        private void listName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Selected Path ===> SelPath
+            string SelPath = Path[listName.SelectedIndex];
+            if (File.Exists(SelPath))
+            {
+                axWMP.URL = SelPath;
+            }
+        }
+    }
+}
